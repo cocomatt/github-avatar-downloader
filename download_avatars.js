2d4827151a0a95188631a5b59bfe9a70d3f93197
cocomatt <matt.currie@outlook.com>
2018-01-23 21:38:29 +0000

    another commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,8 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+request('https://api.github.com/repos/jquery/jquery/contributors', function(err, res, body) {
+  console.log(res);
+});
+

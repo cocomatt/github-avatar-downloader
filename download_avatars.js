bde4e70abee3b00f58c53a175ba7ffc4b6ea5b27
cocomatt <matt.currie@outlook.com>
2018-01-23 22:17:42 +0000

    another commit with updated .gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,21 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-request('https://api.github.com/repos/jquery/jquery/contributors', function(err, res, body) {
-  console.log(res);
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
 });
 
